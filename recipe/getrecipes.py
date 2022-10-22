5a386978cab878075f10a5d238952c46ce6ae112
karanmenon <krmenon@usc.edu>
2022-10-22 18:23:45 -0400

    dataset for NLP

--- recipe/getrecipes.py
+++ recipe/getrecipes.py
@@ -1,4 +1,5 @@
 import http.client
+import json
 conn = http.client.HTTPSConnection("edamam-recipe-search.p.rapidapi.com")
 
 headers = {
@@ -7,9 +8,28 @@ headers = {
     }
 def getRecipe(search):
     query = "/search?q=" + search
+    query = query.replace(" ", "%20")
     conn.request("GET", query, headers=headers)
     res = conn.getresponse()
     data = res.read()
-    print(data.decode("utf-8"))
+    json_data = data.decode("utf-8")
+    raw_data = json.loads(json_data)
+    return(raw_data["hits"])
 
-getRecipe("paneer")
+'''
+foods = ["pizza", "pasta", "salad", "fried rice", "sandwich", "burrito", "taco", "enchilada", "noodle", "sausage", "hot dog", "paneer", "indian", "pad thai", "tikka masala", 
+"filipino", "soup", "cake", "pudding", "pie", "pretzel", "burger", "pancake", "waffle", "sub", "panini", "risotto", "polenta", "paella", "cookie", "green curry", "red curry"
+, "guacamole", "salsa", "queso", "chicken", "turkey", "stuffing", "corn bread", "falafel", "hummus", "kabob", "trinidadian", "afghan", "nachos", "ice cream", "filet mignon", 
+"lasagna", "dumplings", "chorizo", "barbecue", "macaroni", "fried chicken", "jerk", "biscuit", "tofu", "baklava", "samosa", "couscous", "quinoa", "yogurt", "stuffed pepper", "egg"]
+
+with open("ingredients.txt", "w") as f:
+    for dish in foods:
+        raw_data = getRecipe(dish)
+        for recipe in raw_data:
+            ingrList = recipe["recipe"]["ingredientLines"]
+            for ingr in ingrList:
+                f.write(ingr + "\n") 
+'''
+
+
+#getRecipe("pasta")
